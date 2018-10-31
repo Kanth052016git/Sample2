fff324f11133c4821a23c966753c4fda55ffe48a
Kanth052016git <19247345+Kanth052016git@users.noreply.github.com>
2018-10-31 09:31:45 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<body>
+<form action=”login.php” method=”POST”>
+<input type=”text” name=”user”>
+<input type=”pass” name=”pass”>
+<input type=”submit” name=”submit”>
+</form>
+</body>
+</html>
+<?php
+session_start();
+$user = $_POST[‘user’];
+$pass = $_POST[‘pass’];
+if(isset($_POST[‘submit’]))
+{
+if($user==”me” AND $pass==123)
+{
+header(“Location:login2.php”);
+}
+else
+{
+echo “Wrong Username / Password.”;
+}
+}
+?>
