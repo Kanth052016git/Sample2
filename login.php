872cdcd4fbf4aedec6e324b7357dc1910d33b70e
Kanth052016git <19247345+Kanth052016git@users.noreply.github.com>
2018-11-12 01:50:04 -0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <body>
-<form action=”login.php” method=”POST”>
-<input type=”text” name=”user”>
-<input type=”pass” name=”pass”>
-<input type=”submit” name=”submit”>
+<form action="login.php" method="POST">
+<input type="text" name="user">
+<input type="pass" name="pass">
+<input type="submit" name="submit">
 </form>
 </body>
 </html>
 <?php
 session_start();
-$user = $_POST[‘user’];
-$pass = $_POST[‘pass’];
-if(isset($_POST[‘submit’]))
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+if(isset($_POST['submit']))
 {
-if($user==”me” AND $pass==123)
+if($user=="me" AND $pass==123)
 {
-header(“Location:login2.php”);
+header("Location:login2.php");
 }
 else
 {
-echo “Wrong Username / Password.”;
+echo "Wrong Username / Password.";
 }
 }
 ?>
